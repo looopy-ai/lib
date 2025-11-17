629b58deff70fae82d775881dc47615b68161f15
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2025-11-17 16:30:11 +1300

    fixes

--- packages/core/src/stores/messages/index.ts
+++ packages/core/src/stores/messages/index.ts
@@ -4,7 +4,6 @@
  * Design: design/message-management.md
  */
 
-export * from './bedrock-memory-store';
 export * from './hybrid-message-store';
 export * from './interfaces';
 export * from './mem0-message-store';
