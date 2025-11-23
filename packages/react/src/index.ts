c1d3351765b57ae601501e5acf63cc38ca5fa6c2
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2025-11-23 20:28:13 +1300

    feat: Remove duplicate exports of LucideIcon components in index.ts

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,12 +1,12 @@
 export {
+  LucideIcon,
+  type LucideIconName,
+  type LucideIconProps,
+} from './components/lucide-icon';
+export {
   ScrollContainer,
   type ScrollContainerContextValue,
   type ScrollContainerProps,
   type ScrollContainerRenderProps,
   useScrollContainer,
 } from './components/scroll-container';
-export {
-  LucideIcon,
-  type LucideIconName,
-  type LucideIconProps,
-} from './components/lucide-icon';
